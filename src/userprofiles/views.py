6b6c875e3e7510715a14b34c793f46b0c638667d
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2016-03-04 21:25:50 +0500

    Successfully posting registration data to server and validating it

--- src/userprofiles/views.py
+++ src/userprofiles/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import RegistrationForm
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        print "You have been registered sucessfully"
-        return render(request, "subscribe.html", {})
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            print "You have been registered sucessfully"
+            return render(request, "subscribe.html", {})
+        else:
+            print form.errors
