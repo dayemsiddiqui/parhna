cfdcbc1a764af3abc49fd2a06f1d764f20c2334c
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2016-03-05 20:05:38 +0500

    User Login Functionality Added

--- src/userprofiles/urls.py
+++ src/userprofiles/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'register', views.register, name='register'),
     url(r'profile', views.profile, name='profile'),
+    url(r'login', views.signin, name='signin'),
+    url(r'logout', views.signout, name='signout'),
 ]
--- src/userprofiles/views.py
+++ src/userprofiles/views.py
@@ -1,15 +1,60 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
+            user = User.objects.create_user(form.cleaned_data['username'], form.cleaned_data['email'], form.cleaned_data['password'])
+            user.save()
             print "You have been registered sucessfully"
-            return render(request, "subscribe.html", {})
+            return HttpResponseRedirect("/accounts/login")
         else:
+            return render(request, "index.html", {'form' : form})
             print form.errors
+    else:
+        return HttpResponseRedirect("/")
 
+#Displays the login page
+def signin(request):
+    return render(request, "login.html", {})
+
+#Logout the user
+def signout(request):
+    logout(request)
+    return HttpResponseRedirect("/")
+
+#Authenticates the user and takes them to the dashboard
 def profile(request):
-    return render(request, "profile.html", {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return display_profile(request)
+            else:
+                print "Your account is not active yet!"
+                # Return a 'disabled account' error message
+        else:
+            print "Invalid username or password"
+            return HttpResponseRedirect("/accounts/login")
+    else:
+        if request.user.is_authenticated():
+                return display_profile(request)
+        else:
+            return HttpResponseRedirect("/accounts/login")
+
+
+#display and render dashboard
+def display_profile(request):
+    context = {
+    'username': request.user.username,
+    'email': request.user.email
+    }
+    return render(request, "profile.html", context)
